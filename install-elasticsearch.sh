35d707bbf57ce27b2717f4b7e74cf810fdc50602
qrg <qualle.rhythmus+d@gmail.com>
2021-07-19 22:07:19 +0900

    WIP

---
+++ install-elasticsearch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker pull docker.elastic.co/elasticsearch/elasticsearch:7.13.3
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.13.3
